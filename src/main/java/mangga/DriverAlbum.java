f636280ffd5e5567984852a14d9239aa5bd0bc6f
Gregkids <79739949+Gregkids@users.noreply.github.com>
2022-06-30 12:54:42 +0700

    Update DriverAlbum.java

--- src/main/java/mangga/DriverAlbum.java
+++ src/main/java/mangga/DriverAlbum.java
@@ -18,6 +18,10 @@ public class DriverAlbum
         
         song1.play();
         song1.play();
+        song1.play();
+        song1.play();
+        song1.play();
+        song1.play();
         
         song2.setTitle("Photography");
         song2.setArtist("Ed Sheeran");
