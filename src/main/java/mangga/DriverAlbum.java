dbcc89bc0f6657b143fafc37d3717fb33112d430
Gregkids <79739949+Gregkids@users.noreply.github.com>
2022-06-30 13:16:30 +0700

    Update DriverAlbum.java

--- src/main/java/mangga/DriverAlbum.java
+++ src/main/java/mangga/DriverAlbum.java
@@ -15,11 +15,6 @@ public class DriverAlbum
         song1.setArtist("Budi Doremi");
         song1.setDuration(240);
         song1.play();
-        song1.play();
-        song1.play();
-        song1.play();
-        song1.play();
-        song1.play();
         
         song2.setTitle("Photography");
         song2.setArtist("Ed Sheeran");
